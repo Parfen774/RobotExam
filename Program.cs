5d17cc7d20cc181712365987aaf0668d3908af27
Parfen774 <113260962+Parfen774@users.noreply.github.com>
2023-01-31 12:46:48 +0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Examination
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
