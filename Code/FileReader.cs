5d4b8627653e4b77aa4e85857b6b961690c1451f
Parfen774 <113260962+Parfen774@users.noreply.github.com>
2023-01-31 12:50:28 +0500

    Task Completed

---
+++ Code/FileReader.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace Examination
+{
+    internal class FileReader
+    {
+        public int xRobotPos { get; set; }
+        public int yRobotPos { get; set;}
+        public int xEndPos { get; set; }
+        public int yEndPos { get; set; }
+        public string[] operations { get; set; }
+
+        public FileReader(string path)
+        {
+            string[] file = File.ReadAllLines("input.txt");
+
+            xRobotPos = int.Parse(file[0].Split(";")[0]);
+            yRobotPos = int.Parse(file[0].Split(";")[1]);
+
+            xEndPos = int.Parse(file[1].Split(";")[0]);
+            yEndPos = int.Parse(file[1].Split(";")[1]);
+
+            operations = file[2..];
+        }
+    }
+}
---
+++ Code/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Examination
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            FileReader file = new FileReader("input.txt");
+
+            Position posRobot = new Position(file.xRobotPos, file.yRobotPos);
+            Position endPostion = new Position(file.xEndPos, file.yEndPos);
+
+            Robot robot = new Robot(posRobot);
+            robot.Run(file.operations);
+
+            Console.WriteLine(Position.Compare(posRobot, endPostion));
+        }
+    }
+}
